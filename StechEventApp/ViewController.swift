4b5e3266ff4e7ca5baccf927a6e90485aaee2384
Eiji <64912886+Eiji127@users.noreply.github.com>
2020-12-27 12:22:51 +0900

    Initial Commit

---
+++ StechEventApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StechEventApp
+//
+//  Created by 白数叡司 on 2020/12/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
